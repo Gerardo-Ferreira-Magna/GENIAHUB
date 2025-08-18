4808cc7df9727cd30af83876436c1bf532703a71
Gerardo Ferreira <gerardo.ferreira.m@gmail.com>
2025-08-17 20:22:30 -0400

    vista_docente

--- BaseDjango/urls.py
+++ BaseDjango/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('docente/', views.docente, name='docente'),
+
 ]
